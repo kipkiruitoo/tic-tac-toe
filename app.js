7bbd9b5dee996e465b585d28cfaeb3637061ab0f
Davis <daviskipkiruitoo@gmail.com>
2021-06-28 15:35:09 +0300

    finished the project

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 let board = [
-    ['X', 'X', 'X'],
-    ['X', 'X', 'O'],
-    ['X', 'O', 'X']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
 
 ]
 
+let players = ['X', 'O'];
 
 
-let player1 = 'X';
-let player2 = 'O';
+let currentPlayer;
+
+let available = [];
 
 function setup() {
     createCanvas(400, 400)
+
+    currentPlayer = floor(random(players.length))
+
+    for (let j = 0; j < 3; j++) {
+        for (let i = 0; i < 3; i++) {
+                   available.push([i, j]);     
+        }        
+    }
 }
+
+function nextTurn(){
+
+
+    let index = floor(random(available.length));
+    let spot = available.splice(index, 1)[0];
+
+
+    let i = spot[0];
+    let j  = spot[1];
+
+
+    console.log(i, j);
+    board[i] [j] = players[currentPlayer];
+    currentPlayer = (currentPlayer + 1) % players.length;
+}
+
+
 
 
 function draw() {
     background(220)
 
-
+      
     let w = width / 3;
     let h = height / 3;
+
+    line(w, 0,w, height)
+    line(w*2, 0,w*2, height)
+    line(0,h,width, h)
+    line(0,h*2,width, h*2)
     for (let j = 0; j < 3 ;j++) {
         for (let i = 0; i < 3; i++) {
 
@@ -29,16 +62,77 @@ function draw() {
             let spot = board[i][j];
             textSize(32)
             strokeWeight(4)
-            if (spot == player2) {
+            if (spot == players[1]) {
                 noFill()
-                // ellipseMode(CORNER)
                 ellipse(x,y,w / 2);
-            } else if(spot == player1){
+            } else if(spot == players[0]){
                 let xr = w / 4;
                 line(x - xr,y-xr, x+xr, y+xr)
                 line(x + xr, y - xr, x -xr, y+xr)
             }
-            // text(spot, x, y);
         } 
     }
+
+  
+   let result =  checkWinner();
+
+   if(result != null){
+       noLoop();
+       console.log(result)
+   }else{
+       nextTurn();
+   }
+
+    
+}
+
+
+function checkWinner() {
+
+    let winner = null
+
+
+    // check horizontal
+
+    for (let i = 0; i < 3; i++) {
+       
+         if(equals3(board[i][0], board[i][1],board[i][2])){
+            winner = board[i][0];
+    }
+    }
+
+
+    // check vertical
+
+    
+    for (let i = 0; i < 3; i++) {
+       
+         if(equals3(board[0][i] , board[1][i] ,board[2][i])){
+            winner = board[0][i];
+    }
+    }
+
+
+    // check diagonal
+
+    if (equals3(board[0][0] , board[1][1] , board[2][2])) {
+        winner = board[0][0];
+    }
+
+      if (equals3(board[2][0],board[1][1], board[0][2])) {
+        winner = board[2][0];
+    }
+   
+    if (winner == null && available.length == 0) {
+        return 'tie';
+    }else{
+        return winner;
+    }
+
+    
+}
+
+
+function equals3(a,b,c){
+    return (a==b && b ==c && a == c &&  a!= '');
 }
